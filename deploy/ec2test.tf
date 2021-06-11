c99a37eb70a8987ebb33553d7c6afac566bb01e3
ilmoi <iljamoi@protonmail.com>
2021-06-11 12:39:52 +0300

    testing fe build

---
+++ deploy/ec2test.tf
@@ -0,0 +1,17 @@
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-2.0.*-x86_64-gp2"]
+  }
+  owners = ["amazon"]
+}
+
+resource "aws_instance" "ec2test" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t2.micro"
+  tags = merge(
+    local.common_tags,
+    tomap({ Name = "testec2" })
+  )
+}
--- deploy/main.tf
+++ deploy/main.tf
@@ -1,15 +1,29 @@
 terraform {
   // we're keeping state in the cloud rather than on local machine
   backend "s3" {
-    bucket = "solwtf-terraform-bucket"
-    key = "solwtf.tfstate"
-    region = "us-east-1"
-    encrypt = true
+    bucket         = "solwtf-terraform-bucket"
+    key            = "solwtf.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
     dynamodb_table = "solwtf-terra-lock"
+  }
+  // this is the new way of instantiating providers post 0.13 - https://www.terraform.io/docs/language/providers/requirements.html
+  required_providers {
+    aws = {
+      source  = "aws"
+      version = "~> 3.45.0"
+    }
+  }
+}
+
+locals {
+  common_tags = {
+    Environment = terraform.workspace
+    Project     = var.project
+    ManagedBy   = "Terraform"
   }
 }
 
 provider "aws" {
   region = "us-east-1"
-  version = "~> 3.45.0"
 }
